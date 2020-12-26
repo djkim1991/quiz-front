cb2aabfbe55f79f600e24d0f0359f925763e1cda
djkim1991 <djkim1991@naver.com>
2020-12-26 17:43:28 +0900

    update : [마크업] gnb, footer 기본 마크업 추가

--- src/QuizRouter.js
+++ src/QuizRouter.js
@@ -4,16 +4,22 @@ import Home from "./components/pages/home/Home";
 import CreateQuiz from "./components/pages/createQuiz/CreateQuiz";
 import StartQuiz from "./components/pages/startQuiz/StartQuiz";
 import PageNorFound from "./components/errors/PageNotFound";
+import Gnb from "./components/layout/Gnb";
+import Footer from "./components/layout/Footer";
 
 function QuizRouter() {
   return (
-    <div className="main">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/createQuiz" component={CreateQuiz} />
-        <Route path="/startQuiz" component={StartQuiz} />
-        <Route path="/" component={PageNorFound} />
-      </Switch>
+    <div className="layout">
+      <Gnb />
+      <div className="main">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/createQuiz" component={CreateQuiz} />
+          <Route path="/startQuiz" component={StartQuiz} />
+          <Route path="/" component={PageNorFound} />
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layout/Gnb.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Gnb() {
+  return (
+    <div className="gnb">
+      <span className="logo">단퀴즈</span>
+    </div>
+  );
+}
+
+export default Gnb;
