bc941718a3669e77fe81357036b3be1a9ec7eca6
djkim1991 <djkim1991@naver.com>
2020-12-27 01:27:43 +0900

    update : [마크업] 퀴즈 만들기 버튼 마크업 공통으로 변경

--- src/components/pages/home/components/CreateQuizBtn.js
+++ src/components/pages/home/components/CreateQuizBtn.js
@@ -3,7 +3,7 @@ import Link from "../../../common/navi/Link";
 
 function CreateQuizBtn() {
   return (
-    <div className="create-quiz-btn">
+    <div className="btn-type1">
       <Link to="/createQuiz">
         퀴즈 만들기
       </Link>
