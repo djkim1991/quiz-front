fc6b60fa3541962ba7e89a3fb9a3aae5648bfa19
djkim1991 <djkim1991@naver.com>
2021-02-07 23:29:15 +0900

    update : [카카오톡] 계정 로그인 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
 import QuizRouter from "./QuizRouter";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <QuizRouter />
-    </BrowserRouter>
-  );
+class App extends Component {
+  componentDidMount() {
+    window.Kakao.init("20de6ffbd12f76b6f1a92d3ba51c2e9a");
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <QuizRouter/>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/components/pages/home/components/CreateQuizBtn.js
+++ src/components/pages/home/components/CreateQuizBtn.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import FloatBottomBtn from "../../../common/button/FloatBottomBtn";
+require('../../../../external/kakao/kakao.min.js');
 
 function CreateQuizBtn() {
+  function handleClick() {
+    const kakaoAccessToken = window.Kakao.Auth.getAccessToken();
+    console.log(kakaoAccessToken);
+
+    window.Kakao.Auth.login({
+      success() {
+        alert('로그인 성공');
+        window.location.pathname = "/createQuiz";
+      },
+      fail(e) {
+        console.log(e);
+      }
+    });
+  }
+
   return (
-    <FloatBottomBtn to="/createQuiz" text="퀴즈 만들기" />
+    <div>
+      <FloatBottomBtn  text="퀴즈 만들기" handleClick={handleClick} />
+    </div>
   );
 }
 
