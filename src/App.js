1058d38bf3d4741fa3192e989c5d8dd05270557c
djkim1991 <djkim1991@naver.com>
2021-02-13 23:02:09 +0900

    remove : [카카오톡] 카카오 SDK 제거

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import QuizRouter from "./QuizRouter";
 
 class App extends Component {
   componentDidMount() {
-    window.Kakao.init("20de6ffbd12f76b6f1a92d3ba51c2e9a");
   }
 
   render() {
--- src/components/pages/home/components/CreateQuizBtn.js
+++ src/components/pages/home/components/CreateQuizBtn.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import FloatBottomBtn from "../../../common/button/FloatBottomBtn";
-require('../../../../external/kakao/kakao.min.js');
 
 function CreateQuizBtn() {
   function handleClick() {
-    const kakaoAccessToken = window.Kakao.Auth.getAccessToken();
-    console.log(kakaoAccessToken);
-
-    window.Kakao.Auth.login({
-      success() {
-        alert('로그인 성공');
-        window.location.pathname = "/createQuiz";
-      },
-      fail(e) {
-        console.log(e);
-      }
-    });
   }
 
   return (
